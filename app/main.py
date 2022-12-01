5bf9859fe561c07c2ed1a756dc39d4a6098bc9ad
urian <urian1213viera@gmail.com>
2022-11-30 20:58:24 -0500

    agregando archivo requirement.txt

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, render_template
+#Importando flask y algunos paquetes(modulos)
+from flask import Flask, render_template, url_for, redirect
+from datetime import datetime
 from confiDB import * #Importando conexion BD
 
+''' 
+La variable __name__ se pasa como primer argumento al crear una instancia del objeto 
+Flask ( una aplicación Python Flask ). En este caso, __name__ representa el nombre 
+del paquete de aplicaciones y Flask lo utiliza para identificar recursos como plantillas, 
+activos estáticos y la carpeta de instancias.
+'''
+app = Flask(__name__) #Creando la aplicacion 
 
-app = Flask(__name__) 
-
-#Creando mi Decorador para el Home
+#Creando mi primer Decorador o ruta para el Home
 @app.route('/', methods=['GET','POST'])
 def inicio():
     conexion_MySQLdb = connectionBD() #Hago instancia a mi conexión desde la función
@@ -19,5 +26,12 @@ def inicio():
 
 
 
+#Redireccionando cuando la página no existe
+@app.errorhandler(404)
+def not_found(error):
+        return redirect(url_for('inicio'))
+    
+
+#Arrancando la aplicacion    
 if __name__ == '__main__': 
     app.run(debug=True, port=5000) 
